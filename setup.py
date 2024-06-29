fd442bc5a41ce64caed95112130446a246b059d1
ramy-ibrahim-ahmed <ramyibrahim987@gmail.com>
2024-06-29 05:42:28 +0300

    setup local package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.0.0",
+    author="Ramy Barhoma",
+    author_email="cnioxxx@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
