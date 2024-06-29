5ef5efb2b0ea14f53fa14dd9af3a9831cebef90b
ramy-ibrahim-ahmed <ramyibrahim987@gmail.com>
2024-06-29 13:54:16 +0300

    host and port

--- app.py
+++ app.py
@@ -56,5 +56,7 @@ def get_response():
     return jsonify({"response": str(result["result"])})
 
 
+host = '0.0.0.0'
+port = 5000
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host=host, port=port)
